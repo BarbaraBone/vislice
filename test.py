e508824c3f1497a8527c24b0d9887bf6a01b478c
Barbara Bone <barbara.bone@student.fmf.uni-lj.si>
2019-04-24 15:09:28 +0200

    želimo napako

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 # Napiši program, ki izpiše prvih 200 praštevil
 
-def je_prastevilo(n):
+def je_pqaswdfghjkrastevilo(n):
     for i in range(2, n):
         if n % i == 0:
-            return False
     return True
 
 for x in range(1, 201):
     if je_prastevilo(x):
         print(x)
+
+
+        danes nam gre odlično
+        xcvbnkmlčćasfd
